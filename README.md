001dc2dd2171007c60d3cb5c580a18601a86e1a9
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-04-04 06:37:44 +0700

    fix bugs according to teacher comments

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Night Owls Detector
 
-The app shows users from [DEVMAN.org](https://devman.org) who sent tasks for check after midnight (from 0 to 1 am)
+The app shows users from [DEVMAN.org](https://devman.org) who sent tasks for check after midnight (from 0 to 6 am)
 
 # How to Install
 
@@ -19,19 +19,38 @@ To use run the app from command line
 
 ```bash
 $ python seek_dev_nighters.py
-Users who sent the tasks from 0 to 1 am:
+Users who sent the tasks from 0 to 6 am:
 
 constantinovich
-2019-04-02T00:43:17.652092+07:00
-Asia/Novosibirsk
+2019-04-03 05:37:06.222063+07:00
+2019-04-02 00:43:17.652092+07:00
+2019-03-26 00:31:10.020308+07:00
+2019-03-19 00:14:57.412569+07:00
+2019-03-13 00:03:29.959189+07:00
+2019-03-07 00:08:49.039497+07:00
 
 ekluev
-2019-03-29T00:33:43.502227+03:00
-Europe/Moscow
+2019-03-29 00:33:43.502227+03:00
+2019-03-29 00:21:41.053172+03:00
+2019-03-29 00:17:24.343396+03:00
+2019-03-29 00:11:10.910401+03:00
 
-ekluev
-2019-03-29T00:21:41.053172+03:00
-Europe/Moscow
+1d964ba0d0604b37
+2019-03-20 00:00:14.665558+03:00
+
+PeterChe
+2019-04-04 00:31:38.820442+02:00
+
+seregan777
+2019-03-30 05:59:10.236078+03:00
+
+idmedvedevivan
+2019-03-05 00:49:31.430813+03:00
+
+andreishkilev1993
+2019-04-02 03:27:04.253617+03:00
+2019-04-02 01:39:38.527911+03:00
+2019-03-30 02:02:28.221595+03:00
 ```
 
 # Project Goals
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -2,62 +2,55 @@ import requests
 import datetime
 import sys
 import pytz
+from collections import defaultdict
 
-url_template = 'http://devman.org/api/challenges/solution_attempts/'
-
-
-def get_number_of_pages():
-    try:
-        response = requests.get(url_template)
-        return int(response.json()['number_of_pages'])
-    except (
-            requests.exceptions.ConnectionError,
-            requests.exceptions.ConnectTimeout,
-    ):
-        return
+URL = 'http://devman.org/api/challenges/solution_attempts/'
+START_HOUR = 0
+END_HOUR = 6
 
 
 def load_attempts():
-    pages = get_number_of_pages()
-    if not pages:
-        return
-    for page in range(1, pages + 1):
+    page = 1
+    while True:
         params = {
             'page': page,
         }
         try:
-            response = requests.get(url_template, params)
-            attempts = response.json()['records']
+            response = requests.get(URL, params)
+            attempts = response.json()
         except (
                 requests.exceptions.ConnectionError,
                 requests.exceptions.ConnectTimeout,
         ):
             return
 
-        for attempt in attempts:
-            yield attempt
+        yield from attempts['records']
+
+        if page < attempts['number_of_pages']:
+            page += 1
+        else:
+            break
 
 
-def get_midnighters(attempts):
+def get_midnighters(attempts, start_hour, end_hour):
+    midnighters = defaultdict(list)
     for attempt in attempts:
         tz = pytz.timezone(attempt['timezone'])
         attempt_datetime = datetime.datetime.fromtimestamp(
             attempt['timestamp'],
             tz=tz,
         )
-        if attempt_datetime.time().hour == 0:
-            yield {
-                'username': attempt['username'],
-                'time': attempt_datetime.isoformat(),
-                'timezone': attempt['timezone'],
-            }
+        if attempt_datetime.hour in range(start_hour, end_hour):
+            midnighters[attempt['username']].append(attempt_datetime)
+    return midnighters
 
 
-def print_midnighter(midnighter):
-    print(midnighter['username'])
-    print(midnighter['time'])
-    print(midnighter['timezone'])
-    print()
+def print_midnighters(midnighters):
+    for midhighter, attempts_time_list in midnighters.items():
+        print(midhighter)
+        for attempt_time in attempts_time_list:
+            print(attempt_time)
+        print()
 
 
 if __name__ == '__main__':
@@ -66,7 +59,10 @@ if __name__ == '__main__':
     if attempts is None:
         sys.exit("Connection error or server doesn't answer")
 
-    print('Users who sent the tasks from 0 to 1 am:\n')
+    print('Users who sent the tasks from {} to {} am:\n'.format(
+        START_HOUR,
+        END_HOUR
+    ))
 
-    for midnighter in get_midnighters(attempts):
-        print_midnighter(midnighter)
+    midnighters = get_midnighters(attempts, START_HOUR, END_HOUR)
+    print_midnighters(midnighters)
