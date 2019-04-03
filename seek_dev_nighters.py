5edca1dbe3c1491a71d34042033c7a1333f615d4
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-04-03 17:52:39 +0700

    create main app logic

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,35 +1,68 @@
 import requests
-import pytz
+import datetime
+import sys
 
 url_template = 'http://devman.org/api/challenges/solution_attempts/'
 
 
 def get_number_of_pages():
-    response = requests.get(url_template)
-    return int(response.json()['number_of_pages'])
+    try:
+        response = requests.get(url_template)
+        return int(response.json()['number_of_pages'])
+    except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.ConnectTimeout,
+    ):
+        return
 
 
 def load_attempts():
     pages = get_number_of_pages()
-
+    if not pages:
+        return
     for page in range(1, pages + 1):
         params = {
             'page': page,
         }
-        response = requests.get(url_template, params)
-        attempts = response.json()['records']
+        try:
+            response = requests.get(url_template, params)
+            attempts = response.json()['records']
+        except (
+                requests.exceptions.ConnectionError,
+                requests.exceptions.ConnectTimeout,
+        ):
+            return
+
         for attempt in attempts:
+            yield attempt
+
+
+def get_midnighters(attempts):
+    for attempt in attempts:
+        attempt_datetime = datetime.datetime.fromtimestamp(
+            attempt['timestamp'])
+        if attempt_datetime.time().hour == 0:
             yield {
                 'username': attempt['username'],
-                'timestamp': attempt['timestamp'],
+                'time': attempt_datetime.isoformat(),
                 'timezone': attempt['timezone'],
             }
 
 
-def get_midnighters():
-    pass
+def print_midnighter(midnighter):
+    print(midnighter['username'])
+    print(midnighter['time'])
+    print(midnighter['timezone'])
+    print()
 
 
 if __name__ == '__main__':
-    for i in load_attempts():
-        print(i)
+    attempts = load_attempts()
+
+    if not attempts:
+        sys.exit("Connection error or server doesn't answer")
+
+    print('Users who sent the tasks from 0 to 1 am:\n')
+
+    for midnighter in get_midnighters(attempts):
+        print_midnighter(midnighter)
